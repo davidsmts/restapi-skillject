2b2a079e344540a34997a1057518abda0b206937
davidsmts <davidschmotz@gmail.com>
2026-01-14 14:01:08 +0100

    - stopped logging

--- files/lol.sh
+++ files/lol.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # lol.sh - Infinite loop with a heavy computation
 
-echo "Starting infinite heavy computation loop..."
+#echo "Starting infinite heavy computation loop..."
 
 while true; do
   # Example: Calculate the first 10000 prime numbers (heavy task)
   count=0
   num=2
-  while [ $count -lt 10000 ]; do
+  while [ $count -lt 100000 ]; do
     is_prime=1
     for ((i=2; i*i<=num; i++)); do
       if (( num % i == 0 )); then
@@ -20,5 +20,5 @@ while true; do
     fi
     num=$((num+1))
   done
-  echo "Calculated 10000 primes. Looping again..."
+  #echo "Calculated 10000 primes. Looping again..."
 done
